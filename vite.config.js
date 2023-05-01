156918befe15720021b315d1790adcfc00502eef
jscomponent <steffanhalv@gmail.com>
2023-05-01 14:31:41 +0200

    Stored by jscomponent 1682944301571

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,27 @@
 import fs from 'fs'
-import { fileURLToPath, URL } from 'url'
-import { defineConfig } from 'vite'
+import {
+    fileURLToPath,
+    URL
+} from 'url'
+import {
+    defineConfig
+} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import dataUri from '@rollup/plugin-data-uri'
 
-if (fs.existsSync('./cache')) fs.rmSync('./cache', { recursive: true })
+if (fs.existsSync('./cache')) fs.rmSync('./cache', {
+    recursive: true
+})
 
 export default defineConfig({
-  plugins: [vue(), dataUri()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      'vue': 'vue/dist/vue.esm-bundler.js'
+    plugins: [vue(), dataUri()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        },
+        extensions: ['.vue', '.tsx', '.ts', '.mjs', '.js', '.jsx', '.json', '.wasm']
     },
-    extensions: ['.vue', '.tsx', '.ts', '.mjs', '.js', '.jsx', '.json', '.wasm']
-  },
-  define: {
-    '{}': {}
-  }
+    define: {
+        '{}': {}
+    }
 })
