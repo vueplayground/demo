0f6993e95ec11772911c8810c4d6d4a3f82a0b9c
jscomponent <steffanhalv@gmail.com>
2023-05-01 17:40:22 +0200

    Stored by jscomponent 1682955622027

---
+++ src/views/DataDeletion.vue
@@ -0,0 +1,78 @@
+<template>
+    <div>
+        <VueNavigator />
+        <header>
+            <h1>
+                Data Deletion
+            </h1>
+            <p>Updated 28.04.2023</p>
+        </header>
+        <main>
+            <section style="max-width:700px;margin:auto">
+                <p>Vue Playground (“we,” “our,” or “us”) is committed to protecting your privacy. This Privacy Policy explains how your personal information is collected, used, and disclosed by Vue Playground.</p>
+                <p>
+                    This Privacy Policy applies to our website, and its associated subdomains (collectively, our “Service”) alongside our application, Vue Playground.
+                    By accessing or using our Service, you signify that you have read, understood, and agree to our collection, storage, use, and disclosure of your personal information as described in this Privacy Policy and our Terms of Service.
+                </p>
+                <h2>Contact Us</h2>
+                <p>Don't hesitate to contact us if you have any questions.</p><a href="mailto:steffan@siglar.no">steffan@siglar.no</a>
+            </section>
+        </main>
+    </div>
+</template>
+<script>
+    import VueNavigator from "@/components/Navigator.vue";
+    export default {
+        components: {
+            VueNavigator
+        }
+    };
+</script>
+<style>
+    body,
+    html,
+    #app {
+        margin-top: 0px;
+        margin-left: 0px;
+        margin-bottom: 0px;
+        margin-right: 0px;
+    }
+
+    main {
+        display: inline-block;
+        text-align: left;
+        width: 100%;
+        padding: 0px 40px;
+        box-sizing: border-box;
+        margin-bottom: 50px;
+        font-family: verdana;
+        margin-right: auto;
+        margin-left: auto;
+    }
+
+    header {
+        text-align: center;
+        font-family: verdana;
+        padding-top: 10px;
+        padding-bottom: 10px;
+    }
+
+    p {
+        font-size: 20px;
+        color: #6b6b6bff;
+        margin-top: 50px;
+        margin-bottom: 15px;
+    }
+
+    h1 {
+        font-size: 60px;
+        margin-top: 50px;
+        margin-bottom: 30px;
+    }
+
+    h2 {
+        font-size: 30px;
+        margin-top: 100px;
+        text-align: left;
+    }
+</style>
