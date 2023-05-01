6f706b392d665eb8f27f750cfeeb9205bf5ae01a
jscomponent <steffanhalv@gmail.com>
2023-05-01 14:49:02 +0200

    Stored by jscomponent 1682945342890

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,26 @@
+<template>
+    <div>
+        <router-view /><button type="" @click="$router.push('/demo')">
+            Got to demo
+        </button>
+    </div>
+</template>
+<script>
+    export default {
+        data: () => ({})
+    };
+</script>
+<style>
+    body,
+    html,
+    #app {
+        margin-top: 0px;
+        margin-left: 0px;
+        margin-bottom: 0px;
+        margin-right: 0px;
+    }
 
-            <template>
-                <div>
-                    <router-view/>
-                </div>
-            </template>
-            <script>
-            export default {
-                data: () => ({})
-            }
-            </script>
-            <style>
-                body, html, #app {
-                    margin-top: 0px;
-                    margin-left: 0px;
-                    margin-bottom: 0px;
-                    margin-right: 0px;
-                }
-                div {
-                    text-align: center;
-                }
-            </style>
-        
+    div {
+        text-align: center;
+    }
+</style>
