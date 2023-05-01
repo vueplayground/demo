17aa612982d302deeeec6847f7658cbd8686af11
jscomponent <steffanhalv@gmail.com>
2023-05-01 14:20:38 +0200

    Stored by jscomponent 1682943638156

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,12 +1,9 @@
-
-            <template>
-                <div style="margin: auto; font-weight: bold;font-size: 25px; color: navy">
-                    Demo Component In Router
-                </div>
-            </template>
-            <script>
-            export default {}
-            </script>
-            <style scoped>
-            </style>
-        
+<template>
+    <div style="margin: auto; font-weight: bold;font-size: 25px; color: navy">
+        Welcome to VuePlayground!
+    </div>
+</template>
+<script>
+    export default {};
+</script>
+<style scoped></style>
