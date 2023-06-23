5917493f40ef0746451ee65e4a81ff67da1c1074
Vue Playground <steffanhalv@gmail.com>
2023-06-23 17:05:46 +0200

    Stored by Vue Playground 1687532746859

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
-    <footer style="background-color:#474747ff;height:202px;width:100%;text-align:center;box-sizing:border-box;font-size:23px;display:inline-block;padding-top:27px">
+    <footer style="height:202px;width:100%;text-align:center;box-sizing:border-box;font-size:23px;display:inline-block;padding-top:27px;background-image:linear-gradient(204deg, #f0dfe20d 34%, #e1ebe8ff 39%, #f0f5faff 54%);background-color:#edf7f4ff">
 
-        <p style="color:#c9c9c9ff;font-size:16px;margin-bottom:25px;margin-top:30px">
+        <p style="color:#2c4966ff;font-size:16px;margin-bottom:25px;margin-top:30px">
             © All rights reserved - Siglar Development AS {{
 new Date().getUTCFullYear()
 }}
@@ -24,16 +24,16 @@ new Date().getUTCFullYear()
 <style scoped>
     footer {
         font-family: verdana;
-        color: #c9c9c9ff;
+        color: #2c4966ff;
     }
 
     a {
-        color: #c9c9c9ff;
+        color: #2c4966ff;
         text-decoration: none;
         font-size: 16px;
     }
 
     a:hover {
-        color: #ffffffff;
+        color: #000000ff;
     }
 </style>
