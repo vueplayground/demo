836c449728629efa90ede56fb59d09aa8729fe83
Vue Playground <steffanhalv@gmail.com>
2023-06-24 23:59:05 +0200

    Stored by Vue Playground 1687643945093

--- src/views/Features.vue
+++ src/views/Features.vue
@@ -1,4 +1,4 @@
-<template>
+<template v-for="i in 8">
     <div class="bg-slate-100">
         <VueNavigator />
         <header>
@@ -6,82 +6,82 @@
                 Features
             </h1>
             <p style="margin-right:auto;margin-left:auto;max-width:calc(100% - 40px);width:700px">
-                Vue Playground is a full drag & drop builder for vue components and applications
+                Vue Playground is a full drag & drop designer for building vue components and applications.
             </p>
         </header>
         <main style="text-align:center;width:100%;margin-bottom:50px">
+            <section class="p-6 xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 container m-auto grid gap-6">
+                <div class="p-6 inline-block shadow-slate-300 shadow rounded bg-white">
+                    <div class="bg-blue-200 w-12 h-12 rounded pt-2"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-8 h-8 fill-blue-500 m-auto">
+                            <path fill-rule="evenodd" d="M3 6a3 3 0 013-3h12a3 3 0 013 3v12a3 3 0 01-3 3H6a3 3 0 01-3-3V6zm14.25 6a.75.75 0 01-.22.53l-2.25 2.25a.75.75 0 11-1.06-1.06L15.44 12l-1.72-1.72a.75.75 0 111.06-1.06l2.25 2.25c.141.14.22.331.22.53zm-10.28-.53a.75.75 0 000 1.06l2.25 2.25a.75.75 0 101.06-1.06L8.56 12l1.72-1.72a.75.75 0 10-1.06-1.06l-2.25 2.25z" clip-rule="evenodd" />
+                        </svg> </div>
+                    <h3 class="mt-4 mb-3 font-semibold text-2xl text-left">
+                        Code editor
+                    </h3>
+                    <p class="text-slate-500 text-left">
+                        Built in code editor for high flexibility.
+                    </p>
+                </div>
+                <div class="p-6 inline-block shadow-slate-300 shadow rounded bg-white">
+                    <div class="bg-blue-200 w-12 h-12 rounded pt-2"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-8 h-8 fill-blue-500 m-auto">
+                            <path fill-rule="evenodd" d="M16.098 2.598a3.75 3.75 0 113.622 6.275l-1.72.46V12a.75.75 0 01-.22.53l-.75.75a.75.75 0 01-1.06 0l-.97-.97-7.94 7.94a2.56 2.56 0 01-1.81.75 1.06 1.06 0 00-.75.31l-.97.97a.75.75 0 01-1.06 0l-.75-.75a.75.75 0 010-1.06l.97-.97a1.06 1.06 0 00.31-.75c0-.68.27-1.33.75-1.81L11.69 9l-.97-.97a.75.75 0 010-1.06l.75-.75A.75.75 0 0112 6h2.666l.461-1.72c.165-.617.49-1.2.971-1.682zm-3.348 7.463L4.81 18a1.06 1.06 0 00-.31.75c0 .318-.06.63-.172.922a2.56 2.56 0 01.922-.172c.281 0 .551-.112.75-.31l7.94-7.94-1.19-1.19z" clip-rule="evenodd" />
+                        </svg> </div>
+                    <h3 class="mt-4 mb-3 font-semibold text-2xl text-left">
+                        Color picker
+                    </h3>
+                    <p class="text-slate-500 text-left">
+                        Choose colors like a designer and forget the use of hexadecimals.
+                    </p>
+                </div>
+                <div class="p-6 inline-block shadow-slate-300 shadow rounded bg-white">
+                    <div class="bg-blue-200 w-12 h-12 rounded pt-2"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-8 h-8 fill-blue-500 m-auto">
+                            <path d="M21.721 12.752a9.711 9.711 0 00-.945-5.003 12.754 12.754 0 01-4.339 2.708 18.991 18.991 0 01-.214 4.772 17.165 17.165 0 005.498-2.477zM14.634 15.55a17.324 17.324 0 00.332-4.647c-.952.227-1.945.347-2.966.347-1.021 0-2.014-.12-2.966-.347a17.515 17.515 0 00.332 4.647 17.385 17.385 0 005.268 0zM9.772 17.119a18.963 18.963 0 004.456 0A17.182 17.182 0 0112 21.724a17.18 17.18 0 01-2.228-4.605zM7.777 15.23a18.87 18.87 0 01-.214-4.774 12.753 12.753 0 01-4.34-2.708 9.711 9.711 0 00-.944 5.004 17.165 17.165 0 005.498 2.477zM21.356 14.752a9.765 9.765 0 01-7.478 6.817 18.64 18.64 0 001.988-4.718 18.627 18.627 0 005.49-2.098zM2.644 14.752c1.682.971 3.53 1.688 5.49 2.099a18.64 18.64 0 001.988 4.718 9.765 9.765 0 01-7.478-6.816zM13.878 2.43a9.755 9.755 0 016.116 3.986 11.267 11.267 0 01-3.746 2.504 18.63 18.63 0 00-2.37-6.49zM12 2.276a17.152 17.152 0 012.805 7.121c-.897.23-1.837.353-2.805.353-.968 0-1.908-.122-2.805-.353A17.151 17.151 0 0112 2.276zM10.122 2.43a18.629 18.629 0 00-2.37 6.49 11.266 11.266 0 01-3.746-2.504 9.754 9.754 0 016.116-3.985z" />
+                        </svg> </div>
+                    <h3 class="mt-4 mb-3 font-semibold text-2xl text-left">
+                        Github Pages
+                    </h3>
+                    <p class="text-slate-500 text-left">
+                        Deploy your website using GitHub pages with a few simple clicks.
+                    </p>
+                </div>
+                <div class="p-6 inline-block shadow-slate-300 shadow rounded bg-white">
+                    <div class="bg-blue-200 w-12 h-12 rounded pt-2"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-8 h-8 fill-blue-500 m-auto">
+                            <path d="M12.378 1.602a.75.75 0 00-.756 0L3 6.632l9 5.25 9-5.25-8.622-5.03zM21.75 7.93l-9 5.25v9l8.628-5.032a.75.75 0 00.372-.648V7.93zM11.25 22.18v-9l-9-5.25v8.57a.75.75 0 00.372.648l8.628 5.033z" />
+                        </svg> </div>
+                    <h3 class="mt-4 mb-3 font-semibold text-2xl text-left">
+                        Drag and drop
+                    </h3>
+                    <p class="text-slate-500 text-left">
+                        Drag and drop elements using the playground and layers view.
+                    </p>
+                </div>
+                <div class="p-6 inline-block shadow-slate-300 shadow rounded bg-white">
+                    <div class="bg-blue-200 w-12 h-12 rounded pt-2"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-8 h-8 fill-blue-500 m-auto">
+                            <path d="M18 1.5c2.9 0 5.25 2.35 5.25 5.25v3.75a.75.75 0 01-1.5 0V6.75a3.75 3.75 0 10-7.5 0v3a3 3 0 013 3v6.75a3 3 0 01-3 3H3.75a3 3 0 01-3-3v-6.75a3 3 0 013-3h9v-3c0-2.9 2.35-5.25 5.25-5.25z" />
+                        </svg> </div>
+                    <h3 class="mt-4 mb-3 font-semibold text-2xl text-left">
+                        Export source code
+                    </h3>
+                    <p class="text-slate-500 text-left">
+                        You own your files. Connect to GitHub and use without limitations.
+                    </p>
+                </div>
+                <div class="p-6 inline-block shadow-slate-300 shadow rounded bg-white">
+                    <div class="bg-blue-200 w-12 h-12 rounded pt-2"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-8 h-8 fill-blue-500 m-auto">
+                            <path fill-rule="evenodd" d="M10.5 3.75a6 6 0 00-5.98 6.496A5.25 5.25 0 006.75 20.25H18a4.5 4.5 0 002.206-8.423 3.75 3.75 0 00-4.133-4.303A6.001 6.001 0 0010.5 3.75zm2.25 6a.75.75 0 00-1.5 0v4.94l-1.72-1.72a.75.75 0 00-1.06 1.06l3 3a.75.75 0 001.06 0l3-3a.75.75 0 10-1.06-1.06l-1.72 1.72V9.75z" clip-rule="evenodd" />
+                        </svg> </div>
+                    <h3 class="mt-4 mb-3 font-semibold text-2xl text-left">
+                        Export build
+                    </h3>
+                    <p class="text-slate-500 text-left">
+                        Get your distribution ready package and deploy anywhere.
+                    </p>
+                </div>
+            </section>
             <section class="p-6 m-auto container">
-                <div class="xl:columns-4 lg:columns-3 sm:columns-2 columns-1">
-                    <div class="mb-6 p-6 inline-block shadow-slate-300 shadow rounded bg-white">
-                        <div class="bg-blue-200 w-12 h-12 rounded">
-                        </div>
-                        <h3 class="mt-4 mb-3 font-semibold text-2xl text-left">
-                            Marketing A
-                        </h3>
-                        <p class="text-slate-500 text-left">
-                            Plan it, create it, launch it. Collaborate seamlessly with all the organization and hit your marketing goals every month with our marketing plan.
-                        </p>
-                    </div>
-                    <div class="mb-6 p-6 inline-block shadow-slate-300 shadow rounded bg-white">
-                        <div class="bg-blue-200 w-12 h-12 rounded">
-                        </div>
-                        <h3 class="mt-4 mb-3 font-semibold text-2xl text-left">
-                            Marketing B
-                        </h3>
-                        <p class="text-slate-500 text-left">
-                            Plan it, create it, launch it. Collaborate seamlessly with all the organization and hit your marketing goals every month with our marketing plan.
-                        </p>
-                    </div>
-                    <div class="mb-6 p-6 inline-block shadow-slate-300 shadow rounded bg-white">
-                        <div class="bg-blue-200 w-12 h-12 rounded">
-                        </div>
-                        <h3 class="mt-4 mb-3 font-semibold text-2xl text-left">
-                            Marketing C
-                        </h3>
-                        <p class="text-slate-500 text-left">
-                            Plan it, create it, launch it. Collaborate seamlessly with all the organization and hit your marketing goals every month with our marketing plan.
-                        </p>
-                    </div>
-                    <div class="mb-6 p-6 inline-block shadow-slate-300 shadow rounded bg-white">
-                        <div class="bg-blue-200 w-12 h-12 rounded">
-                        </div>
-                        <h3 class="mt-4 mb-3 font-semibold text-2xl text-left">
-                            Marketing
-                        </h3>
-                        <p class="text-slate-500 text-left">
-                            Plan it, create it, launch it. Collaborate seamlessly with all the organization and hit your marketing goals every month with our marketing plan.
-                        </p>
-                    </div>
-                    <div class="mb-6 p-6 inline-block shadow-slate-300 shadow rounded bg-white">
-                        <div class="bg-blue-200 w-12 h-12 rounded">
-                        </div>
-                        <h3 class="mt-4 mb-3 font-semibold text-2xl text-left">
-                            Marketing
-                        </h3>
-                        <p class="text-slate-500 text-left">
-                            Plan it, create it, launch it. Collaborate seamlessly with all the organization and hit your marketing goals every month with our marketing plan.
-                        </p>
-                    </div>
-                    <div class="mb-6 p-6 inline-block shadow-slate-300 shadow rounded bg-white">
-                        <div class="bg-blue-200 w-12 h-12 rounded">
-                        </div>
-                        <h3 class="mt-4 mb-3 font-semibold text-2xl text-left">
-                            Marketing
-                        </h3>
-                        <p class="text-slate-500 text-left">
-                            Plan it, create it, launch it. Collaborate seamlessly with all the organization and hit your marketing goals every month with our marketing plan.
-                        </p>
-                    </div>
+                <div class="">
                 </div>
                 <div class="feature-list" @click="open('https://vueplay.app')" style="cursor:pointer">
-                    <div>Code editor</div>
-                    <div>Color selector</div>
-                    <div>Deploy to GitHub pages</div>
-                    <div>Drag & Drop components</div>
-                    <div>Drag & Drop elements</div>
-                    <div>
-                        Export build
-                    </div>
                     <div>
                         Export project
                     </div>
