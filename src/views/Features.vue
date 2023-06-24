23878b4f67875d2e99d676521966f106a6dc696d
Vue Playground <steffanhalv@gmail.com>
2023-06-25 00:13:12 +0200

    Stored by Vue Playground 1687644792061

--- src/views/Features.vue
+++ src/views/Features.vue
@@ -16,7 +16,7 @@
                             <path d="M12.378 1.602a.75.75 0 00-.756 0L3 6.632l9 5.25 9-5.25-8.622-5.03zM21.75 7.93l-9 5.25v9l8.628-5.032a.75.75 0 00.372-.648V7.93zM11.25 22.18v-9l-9-5.25v8.57a.75.75 0 00.372.648l8.628 5.033z" />
                         </svg> </div>
                     <h3 class="mt-4 mb-3 font-semibold text-2xl text-left">
-                        Drag and drop
+                        Drag and Drop
                     </h3>
                     <p class="text-slate-500 text-left">
                         Drag and drop elements using the playground and layers view.
@@ -27,10 +27,21 @@
                             <path fill-rule="evenodd" d="M16.098 2.598a3.75 3.75 0 113.622 6.275l-1.72.46V12a.75.75 0 01-.22.53l-.75.75a.75.75 0 01-1.06 0l-.97-.97-7.94 7.94a2.56 2.56 0 01-1.81.75 1.06 1.06 0 00-.75.31l-.97.97a.75.75 0 01-1.06 0l-.75-.75a.75.75 0 010-1.06l.97-.97a1.06 1.06 0 00.31-.75c0-.68.27-1.33.75-1.81L11.69 9l-.97-.97a.75.75 0 010-1.06l.75-.75A.75.75 0 0112 6h2.666l.461-1.72c.165-.617.49-1.2.971-1.682zm-3.348 7.463L4.81 18a1.06 1.06 0 00-.31.75c0 .318-.06.63-.172.922a2.56 2.56 0 01.922-.172c.281 0 .551-.112.75-.31l7.94-7.94-1.19-1.19z" clip-rule="evenodd" />
                         </svg> </div>
                     <h3 class="mt-4 mb-3 font-semibold text-2xl text-left">
-                        Color picker
+                        Color Picker
                     </h3>
                     <p class="text-slate-500 text-left">
                         Choose colors like a designer and forget the use of hexadecimals.
+                    </p>
+                </div>
+                <div class="p-6 inline-block shadow-slate-300 shadow rounded bg-white">
+                    <div class="bg-blue-200 w-12 h-12 rounded pt-2"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-8 h-8 fill-blue-500 m-auto">
+                            <path d="M18.75 12.75h1.5a.75.75 0 000-1.5h-1.5a.75.75 0 000 1.5zM12 6a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5A.75.75 0 0112 6zM12 18a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5A.75.75 0 0112 18zM3.75 6.75h1.5a.75.75 0 100-1.5h-1.5a.75.75 0 000 1.5zM5.25 18.75h-1.5a.75.75 0 010-1.5h1.5a.75.75 0 010 1.5zM3 12a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5A.75.75 0 013 12zM9 3.75a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5zM12.75 12a2.25 2.25 0 114.5 0 2.25 2.25 0 01-4.5 0zM9 15.75a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5z" />
+                        </svg> </div>
+                    <h3 class="mt-4 mb-3 font-semibold text-2xl text-left">
+                        Style Controllers
+                    </h3>
+                    <p class="text-slate-500 text-left">
+                        CSS is more fun when you have the right design tools with easy navigation.
                     </p>
                 </div>
                 <div class="p-6 inline-block shadow-slate-300 shadow rounded bg-white">
@@ -49,7 +60,7 @@
                             <path fill-rule="evenodd" d="M3 6a3 3 0 013-3h12a3 3 0 013 3v12a3 3 0 01-3 3H6a3 3 0 01-3-3V6zm14.25 6a.75.75 0 01-.22.53l-2.25 2.25a.75.75 0 11-1.06-1.06L15.44 12l-1.72-1.72a.75.75 0 111.06-1.06l2.25 2.25c.141.14.22.331.22.53zm-10.28-.53a.75.75 0 000 1.06l2.25 2.25a.75.75 0 101.06-1.06L8.56 12l1.72-1.72a.75.75 0 10-1.06-1.06l-2.25 2.25z" clip-rule="evenodd" />
                         </svg> </div>
                     <h3 class="mt-4 mb-3 font-semibold text-2xl text-left">
-                        Code editor
+                        Code Editor
                     </h3>
                     <p class="text-slate-500 text-left">
                         Built in code editor for high flexibility.
@@ -57,10 +68,21 @@
                 </div>
                 <div class="p-6 inline-block shadow-slate-300 shadow rounded bg-white">
                     <div class="bg-blue-200 w-12 h-12 rounded pt-2"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-8 h-8 fill-blue-500 m-auto">
+                            <path fill-rule="evenodd" d="M4.5 9.75a6 6 0 0111.573-2.226 3.75 3.75 0 014.133 4.303A4.5 4.5 0 0118 20.25H6.75a5.25 5.25 0 01-2.23-10.004 6.072 6.072 0 01-.02-.496z" clip-rule="evenodd" />
+                        </svg> </div>
+                    <h3 class="mt-4 mb-3 font-semibold text-2xl text-left">
+                        Stored in the Cloud
+                    </h3>
+                    <p class="text-slate-500 text-left">
+                        Get access everywhere - safely stored in our database.
+                    </p>
+                </div>
+                <div class="p-6 inline-block shadow-slate-300 shadow rounded bg-white">
+                    <div class="bg-blue-200 w-12 h-12 rounded pt-2"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-8 h-8 fill-blue-500 m-auto">
                             <path d="M18 1.5c2.9 0 5.25 2.35 5.25 5.25v3.75a.75.75 0 01-1.5 0V6.75a3.75 3.75 0 10-7.5 0v3a3 3 0 013 3v6.75a3 3 0 01-3 3H3.75a3 3 0 01-3-3v-6.75a3 3 0 013-3h9v-3c0-2.9 2.35-5.25 5.25-5.25z" />
                         </svg> </div>
                     <h3 class="mt-4 mb-3 font-semibold text-2xl text-left">
-                        Export source code
+                        Export Source Code
                     </h3>
                     <p class="text-slate-500 text-left">
                         You own your files. Connect to GitHub and use without limitations.
@@ -71,44 +93,26 @@
                             <path fill-rule="evenodd" d="M10.5 3.75a6 6 0 00-5.98 6.496A5.25 5.25 0 006.75 20.25H18a4.5 4.5 0 002.206-8.423 3.75 3.75 0 00-4.133-4.303A6.001 6.001 0 0010.5 3.75zm2.25 6a.75.75 0 00-1.5 0v4.94l-1.72-1.72a.75.75 0 00-1.06 1.06l3 3a.75.75 0 001.06 0l3-3a.75.75 0 10-1.06-1.06l-1.72 1.72V9.75z" clip-rule="evenodd" />
                         </svg> </div>
                     <h3 class="mt-4 mb-3 font-semibold text-2xl text-left">
-                        Export build
+                        Export Build
                     </h3>
                     <p class="text-slate-500 text-left">
                         Get your distribution-ready package and deploy anywhere.
                     </p>
                 </div>
+                <div class="p-6 inline-block shadow-slate-300 shadow rounded bg-white">
+                    <div class="bg-blue-200 w-12 h-12 rounded pt-2"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-8 h-8 fill-blue-500 m-auto">
+                            <path fill-rule="evenodd" d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75H12a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z" clip-rule="evenodd" />
+                        </svg> </div>
+                    <h3 class="mt-4 mb-3 font-semibold text-2xl text-left">
+                        Google Fonts
+                    </h3>
+                    <p class="text-slate-500 text-left">
+                        Choose between 1500+ open source fonts from Google.
+                    </p>
+                </div>
             </section>
             <section class="p-6 m-auto container">
                 <div class="">
-                </div>
-                <div class="feature-list" @click="open('https://vueplay.app')" style="cursor:pointer">
-                    <div>
-                        File explorer
-                    </div>
-                    <div>Google fonts selector</div>
-                    <div>
-                        Import assets
-                    </div>
-                    <div>
-                        Import project
-                    </div>
-                    <div>Inline & css styling</div>
-                    <div>
-                        Javascript mockup
-                    </div>
-                    <div>Layers view</div>
-                    <div>Live preview</div>
-                    <div>Push to GitHub</div>
-                    <div>
-                        Responsive designer
-                    </div>
-                    <div>Routing</div>
-                    <div>
-                        Store online
-                    </div>
-                    <div>Transform positioning tool</div>
-                    <div>Transform resize tool</div>
-                    <div>Transform rotate tool</div>
                 </div>
             </section>
             <section><button @click="$router.push('/pricing')">
