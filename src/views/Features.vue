cc1a6428a57b012164deed3713f0f8cab020e74a
Vue Playground <steffanhalv@gmail.com>
2023-06-24 23:20:23 +0200

    Stored by Vue Playground 1687641623927

--- src/views/Features.vue
+++ src/views/Features.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="bg-slate-100">
         <VueNavigator />
         <header>
             <h1>
@@ -8,48 +8,112 @@
             <p style="margin-right:auto;margin-left:auto;max-width:calc(100% - 40px);width:700px">
                 Vue Playground is a full drag & drop builder for vue components and applications
             </p>
-            <div class="feature-list" @click="open('https://vueplay.app')" style="cursor:pointer">
-                <div>Code editor</div>
-                <div>Color selector</div>
-                <div>Deploy to GitHub pages</div>
-                <div>Drag & Drop components</div>
-                <div>Drag & Drop elements</div>
-                <div>
-                    Export build
-                </div>
-                <div>
-                    Export project
-                </div>
-                <div>
-                    File explorer
-                </div>
-                <div>Google fonts selector</div>
-                <div>
-                    Import assets
-                </div>
-                <div>
-                    Import project
-                </div>
-                <div>Inline & css styling</div>
-                <div>
-                    Javascript mockup
-                </div>
-                <div>Layers view</div>
-                <div>Live preview</div>
-                <div>Push to GitHub</div>
-                <div>
-                    Responsive designer
-                </div>
-                <div>Routing</div>
-                <div>
-                    Store online
-                </div>
-                <div>Transform positioning tool</div>
-                <div>Transform resize tool</div>
-                <div>Transform rotate tool</div>
-            </div>
         </header>
         <main style="text-align:center;width:100%;margin-bottom:50px">
+            <section class="p-6 m-auto container">
+                <div class="xl:columns-4 lg:columns-3 sm:columns-2 columns-1">
+                    <div class="mb-6 p-6 inline-block shadow-slate-300 shadow rounded bg-white">
+                        <div class="bg-blue-200 w-12 h-12 rounded">
+                        </div>
+                        <h3 class="mt-4 mb-3 font-semibold text-2xl text-left">
+                            Marketing A
+                        </h3>
+                        <p class="text-slate-500 text-left">
+                            Plan it, create it, launch it. Collaborate seamlessly with all the organization and hit your marketing goals every month with our marketing plan.
+                        </p>
+                    </div>
+                    <div class="mb-6 p-6 inline-block shadow-slate-300 shadow rounded bg-white">
+                        <div class="bg-blue-200 w-12 h-12 rounded">
+                        </div>
+                        <h3 class="mt-4 mb-3 font-semibold text-2xl text-left">
+                            Marketing B
+                        </h3>
+                        <p class="text-slate-500 text-left">
+                            Plan it, create it, launch it. Collaborate seamlessly with all the organization and hit your marketing goals every month with our marketing plan.
+                        </p>
+                    </div>
+                    <div class="mb-6 p-6 inline-block shadow-slate-300 shadow rounded bg-white">
+                        <div class="bg-blue-200 w-12 h-12 rounded">
+                        </div>
+                        <h3 class="mt-4 mb-3 font-semibold text-2xl text-left">
+                            Marketing C
+                        </h3>
+                        <p class="text-slate-500 text-left">
+                            Plan it, create it, launch it. Collaborate seamlessly with all the organization and hit your marketing goals every month with our marketing plan.
+                        </p>
+                    </div>
+                    <div class="mb-6 p-6 inline-block shadow-slate-300 shadow rounded bg-white">
+                        <div class="bg-blue-200 w-12 h-12 rounded">
+                        </div>
+                        <h3 class="mt-4 mb-3 font-semibold text-2xl text-left">
+                            Marketing
+                        </h3>
+                        <p class="text-slate-500 text-left">
+                            Plan it, create it, launch it. Collaborate seamlessly with all the organization and hit your marketing goals every month with our marketing plan.
+                        </p>
+                    </div>
+                    <div class="mb-6 p-6 inline-block shadow-slate-300 shadow rounded bg-white">
+                        <div class="bg-blue-200 w-12 h-12 rounded">
+                        </div>
+                        <h3 class="mt-4 mb-3 font-semibold text-2xl text-left">
+                            Marketing
+                        </h3>
+                        <p class="text-slate-500 text-left">
+                            Plan it, create it, launch it. Collaborate seamlessly with all the organization and hit your marketing goals every month with our marketing plan.
+                        </p>
+                    </div>
+                    <div class="mb-6 p-6 inline-block shadow-slate-300 shadow rounded bg-white">
+                        <div class="bg-blue-200 w-12 h-12 rounded">
+                        </div>
+                        <h3 class="mt-4 mb-3 font-semibold text-2xl text-left">
+                            Marketing
+                        </h3>
+                        <p class="text-slate-500 text-left">
+                            Plan it, create it, launch it. Collaborate seamlessly with all the organization and hit your marketing goals every month with our marketing plan.
+                        </p>
+                    </div>
+                </div>
+                <div class="feature-list" @click="open('https://vueplay.app')" style="cursor:pointer">
+                    <div>Code editor</div>
+                    <div>Color selector</div>
+                    <div>Deploy to GitHub pages</div>
+                    <div>Drag & Drop components</div>
+                    <div>Drag & Drop elements</div>
+                    <div>
+                        Export build
+                    </div>
+                    <div>
+                        Export project
+                    </div>
+                    <div>
+                        File explorer
+                    </div>
+                    <div>Google fonts selector</div>
+                    <div>
+                        Import assets
+                    </div>
+                    <div>
+                        Import project
+                    </div>
+                    <div>Inline & css styling</div>
+                    <div>
+                        Javascript mockup
+                    </div>
+                    <div>Layers view</div>
+                    <div>Live preview</div>
+                    <div>Push to GitHub</div>
+                    <div>
+                        Responsive designer
+                    </div>
+                    <div>Routing</div>
+                    <div>
+                        Store online
+                    </div>
+                    <div>Transform positioning tool</div>
+                    <div>Transform resize tool</div>
+                    <div>Transform rotate tool</div>
+                </div>
+            </section>
             <section><button @click="$router.push('/pricing')">
                     Pricing
                 </button><button @click="open('https://www.vueplay.com')" class="higlighted">
@@ -91,13 +155,6 @@
         font-family: verdana;
         padding-top: 10px;
         padding-bottom: 10px;
-    }
-
-    p {
-        font-size: 20px;
-        color: #6b6b6bff;
-        margin-top: 50px;
-        margin-bottom: 50px;
     }
 
     h1 {
