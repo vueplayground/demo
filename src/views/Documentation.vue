9121116cba3b1a2103399c9ae41c2785a3df7a97
Vue Playground <steffanhalv@gmail.com>
2023-06-26 20:41:31 +0200

    Stored by Vue Playground 1687804891769

--- src/views/Documentation.vue
+++ src/views/Documentation.vue
@@ -6,7 +6,7 @@
                     <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
                     </svg> </label><input class="peer hidden" type="checkbox" id="toggle" />
-                <div class="w-80 max-w-full z-10 bg-blue-300 peer-checked:bg-blue-500 h-full absolute top-0"></div>
+                <div class="duration-200 transition-all ease-in-out w-0 max-w-full z-10 bg-blue-300 peer-checked:w-80 h-full absolute top-0 overflow-auto"></div>
             </div>
         </main>
     </div>
