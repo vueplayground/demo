4415492444a1c5b5d58cd82c7489736563bcade1
Vue Play <steffanhalv@gmail.com>
2023-07-01 23:56:48 +0200

    Stored by Vue Play 1688248608631

--- src/docs/Shortcuts.vue
+++ src/docs/Shortcuts.vue
@@ -6,8 +6,6 @@
             </h1>
             <div class="relative overflow-x-auto shadow-md sm:rounded-lg mt-12">
                 <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                    <caption class="p-5 text-lg font-semibold text-left text-gray-900 bg-white dark:text-white dark:bg-gray-800">
-                    </caption>
                     <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
                             <th scope="col" class="px-6 py-3" style="max-width:400px">
