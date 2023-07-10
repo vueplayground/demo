f8d100b4c777698e0c33beac3312c9b2a6dff5d3
Vue Play <steffanhalv@gmail.com>
2023-07-10 17:23:41 +0200

    Stored by Vue Play 1689002621275

--- src/docs/FAQ.vue
+++ src/docs/FAQ.vue
@@ -2,11 +2,8 @@
     <section class="p-6 pt-20 md:pt-6 pb-40">
         <article class="max-w-2xl mx-auto mb-6 mt-4">
             <h1 class="mb-6 text-4xl">
-                Other
-            </h1>
-            <p class="text-2xl">
                 Frequently Asked Questions
-            </p>
+            </h1>
             <div>
                 <p style="margin-top:15px" class="py-2 text-lg font-medium">
                     Is "vueplay.com" built entirely with Vue Play?
--- src/docs/VBind.vue
+++ src/docs/VBind.vue
@@ -2,11 +2,8 @@
     <section class="p-6 pt-20 md:pt-6 pb-40">
         <article class="max-w-2xl mx-auto mb-6 mt-4">
             <h1 class="mb-6 text-4xl">
-                Limitations
-            </h1>
-            <p class="text-2xl">
                 v-bind
-            </p>
+            </h1>
             <p style="margin-top:15px" class="py-2 text-lg font-medium">
                 Currently v-bind in the stylesheet is not supported.
             </p>
