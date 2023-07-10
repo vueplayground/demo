c47d9051bbda9ac325465d10d4ceacae034b321a
Vue Play <steffanhalv@gmail.com>
2023-07-10 21:05:07 +0200

    Stored by Vue Play 1689015907370

--- src/docs/Shortcuts.vue
+++ src/docs/Shortcuts.vue
@@ -1,6 +1,6 @@
 <template>
     <section class="p-6 pt-20 md:pt-6 pb-40">
-        <article class="max-w-2xl mx-auto mb-6 mt-4 shadow border rounded-lg bg-white px-6 py-12 overflow-auto">
+        <article class="max-w-2xl mx-auto mb-6 mt-4 shadow border rounded-lg bg-white px-12 py-14 overflow-auto">
             <h1 class="mb-6 text-4xl">
                 Keyboard Shortcuts
             </h1>
--- src/docs/VBind.vue
+++ src/docs/VBind.vue
@@ -1,6 +1,6 @@
 <template>
     <section class="p-6 pt-20 md:pt-6 pb-40">
-        <article class="max-w-2xl mx-auto mb-6 mt-4 shadow border rounded-lg bg-white px-6 py-12 overflow-auto">
+        <article class="max-w-2xl mx-auto mb-6 mt-4 shadow border rounded-lg bg-white px-12 py-14 overflow-auto">
             <h1 class="mb-6 text-4xl">
                 v-bind
             </h1>
@@ -10,7 +10,7 @@
             <p style="margin-top:15px" class="text-lg">
                 In the mean time, you can use style binding with the computed hook in the Options API instead:
             </p>
-            <pre style="margin-top:15px" class="text-white rounded bg-slate-900 p-4 text-lg">
+            <pre style="margin-top:15px" class="text-white rounded bg-slate-900 p-4 text-lg overflow-auto">
 <code>
 &lt;script&gt;
 export default() {
