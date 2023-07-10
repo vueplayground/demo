5a6ca8c9491853d3e9260817ffafdddc73837fc3
Vue Play <steffanhalv@gmail.com>
2023-07-10 21:54:59 +0200

    Stored by Vue Play 1689018899794

--- src/logic/router.js
+++ src/logic/router.js
@@ -4,70 +4,74 @@ import {
 } from 'vue-router'
 
 const routes = [{
-    path: '/',
-    component: () => import('@/views/About.vue')
-},
-{
-    path: '/features',
-    component: () => import('@/views/Features.vue')
-},
-{
-    path: '/pricing',
-    component: () => import('@/views/Pricing.vue')
-},
-{
-    path: '/documentation',
-    component: () => import('@/views/Documentation.vue'),
-    children: [{
-        path: '',
-        component: () => import('@/docs/Introduction.vue')
+        path: '/',
+        component: () => import('@/views/About.vue')
     },
     {
-        path: 'shortcuts',
-        component: () => import('@/docs/Shortcuts.vue')
+        path: '/features',
+        component: () => import('@/views/Features.vue')
     },
     {
-        path: 'deployment',
-        component: () => import('@/docs/Deployment.vue')
+        path: '/pricing',
+        component: () => import('@/views/Pricing.vue')
     },
     {
-        path: 'faq',
-        component: () => import('@/docs/FAQ.vue')
+        path: '/documentation',
+        component: () => import('@/views/Documentation.vue'),
+        children: [{
+                path: '',
+                component: () => import('@/docs/Introduction.vue')
+            },
+            {
+                path: 'shortcuts',
+                component: () => import('@/docs/Shortcuts.vue')
+            },
+            {
+                path: 'deployment',
+                component: () => import('@/docs/Deployment.vue')
+            },
+            {
+                path: 'faq',
+                component: () => import('@/docs/FAQ.vue')
+            },
+            {
+                path: 'general',
+                component: () => import('@/docs/General.vue')
+            },
+            {
+                path: 'composition-api',
+                component: () => import('@/docs/CompositionAPI.vue')
+            },
+            {
+                path: 'typescript',
+                component: () => import('@/docs/Typescript.vue')
+            },
+            {
+                path: 'v-bind',
+                component: () => import('@/docs/VBind.vue')
+            }
+        ]
     },
     {
-        path: 'general',
-        component: () => import('@/docs/General.vue')
+        path: '/support',
+        component: () => import('@/views/Support.vue')
     },
     {
-        path: 'composition-api',
-        component: () => import('@/docs/CompositionAPI.vue')
+        path: '/terms-of-service',
+        component: () => import('@/views/TermsOfService.vue')
     },
     {
-        path: 'typescript',
-        component: () => import('@/docs/Typescript.vue')
+        path: '/privacy-policy',
+        component: () => import('@/views/PrivacyPolicy.vue')
     },
     {
-        path: 'v-bind',
-        component: () => import('@/docs/VBind.vue')
+        path: '/data-deletion',
+        component: () => import('@/views/DataDeletion.vue')
+    },
+    {
+        path: '/release-log',
+        component: () => import('@/views/ReleaseLog.vue')
     }
-    ]
-},
-{
-    path: '/terms-of-service',
-    component: () => import('@/views/TermsOfService.vue')
-},
-{
-    path: '/privacy-policy',
-    component: () => import('@/views/PrivacyPolicy.vue')
-},
-{
-    path: '/data-deletion',
-    component: () => import('@/views/DataDeletion.vue')
-},
-{
-    path: '/release-log',
-    component: () => import('@/views/ReleaseLog.vue')
-}
 ]
 
 const router = createRouter({
