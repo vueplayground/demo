f9de73776b23a22ece5f187d63c5630a6f0cd8ce
Vue Play <steffanhalv@gmail.com>
2023-07-11 18:05:13 +0200

    Stored by Vue Play 1689091513615

--- src/components/Meta.vue
+++ src/components/Meta.vue
@@ -1,12 +1,11 @@
-<template>
-    <Teleport to="head title">{{title || 'Vue Play'}}</Teleport>
-</template>
+<template></template>
 <script>
-    export default {
-        props: ['title', 'description'],
-        created() {
-            if (this.description) document.head.children.description = this.description
-        }
+export default {
+    props: ['title', 'description'],
+    created() {
+        document.title = this.title || 'Vue Play'
+        document.head.children.description.content = this.description || 'Design Vue files with a drag & drop template builder. Use playground to create your new web app.'
     }
+}
 </script>
 <style scoped></style>
