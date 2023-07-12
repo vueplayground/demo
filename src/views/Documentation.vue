85d3a1a08751504b4289533f1d9d7f7f6a4865d6
Vue Play <steffanhalv@gmail.com>
2023-07-12 15:53:52 +0200

    Stored by Vue Play 1689170032249

--- src/views/Documentation.vue
+++ src/views/Documentation.vue
@@ -18,9 +18,6 @@
                         <li class="hover:text-slate-900 text-slate-700 cursor-pointer pl-6" @click="$router.push('/documentation'), menu=false" :class="{'font-semibold': $route.path === '/documentation','border-emerald-400': $route.path === '/documentation','border-l-4': $route.path === '/documentation'}" style="height:25px">
                             Introduction
                         </li>
-                        <li @click="$router.push('/documentation/shortcuts'), menu=false" class="hover:text-slate-900 text-slate-700 cursor-pointer pl-6" :class="{'font-semibold': $route.path === '/documentation/shortcuts','border-emerald-400': $route.path === '/documentation/shortcuts','border-l-4': $route.path === '/documentation/shortcuts'}">
-                            Keyboard Shortcuts
-                        </li>
                         <li @click="$router.push('/documentation/deployment'), menu=false" class="hover:text-slate-900 text-slate-700 cursor-pointer pl-6" :class="{'font-semibold': $route.path === '/documentation/deployment','border-emerald-400': $route.path === '/documentation/deployment','border-l-4': $route.path === '/documentation/deployment'}">
                             Deployment
                         </li>
