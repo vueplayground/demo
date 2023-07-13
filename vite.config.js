c5aebe5386f13744bb40f25767366c12bddea611
Vue Play <steffanhalv@gmail.com>
2023-07-13 13:13:35 +0200

    Stored by Vue Play 1689246815869

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,6 @@ import vue from '@vitejs/plugin-vue'
 import dataUri from '@rollup/plugin-data-uri'
 
 export default defineConfig({
-    base: '/demo/',
     plugins: [vue(), dataUri()],
     resolve: {
         alias: {
