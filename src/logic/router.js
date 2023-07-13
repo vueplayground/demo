716d76c5c11c9a0033d14fc9e0b86c995fe663b4
Vue Play <steffanhalv@gmail.com>
2023-07-13 13:31:22 +0200

    Stored by Vue Play 1689247882792

--- src/logic/router.js
+++ src/logic/router.js
@@ -18,7 +18,7 @@ const routes = [{
 ]
 
 const router = createRouter({
-    history: createWebHistory("/demo"),
+    history: createWebHistory(import.meta.env.BASE_URL),
     routes,
     scrollBehavior(to, from, savedPosition) {
         return {
