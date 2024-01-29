8b4cbe7478f7675ddce435aa2c8dda092a16cbb5
Vue Play <author@vueplay.com>
2024-01-29 13:01:22 +0100

    Stored by Vue Play

--- src/components/Navigator.vue
+++ src/components/Navigator.vue
@@ -1,30 +1,4 @@
 <template>
-	<nav class="bg-gray-500 h-12 w-full block top-0 sticky text-right shadow border-slate-300 border-b">
-		<div
-			style="width:83px;background-size:contain;background-repeat:no-repeat;margin-left:10px;cursor:pointer;float:left;height:35px;margin-top:7px;margin-bottom:7px"
-			@click="$router.push('/')"
-			class="rounded-full logo"
-		>
-		</div><button
-			@click="$router.push('/')"
-			:class="{active: route === '/'}"
-			class="text-slate-200 hover:text-slate-50"
-		>
-			Home
-		</button> <button
-			@click="$router.push('/about')"
-			:class="{active: route === '/about'}"
-			class="text-slate-200 hover:text-slate-50"
-		>
-			About
-		</button><button
-			@click="$router.push('/contact')"
-			:class="{active: route === '/contact'}"
-			class="text-slate-200 hover:text-slate-50"
-		>
-			Contact
-		</button>
-	</nav>
 	<TopMenu>
 		<li>
 			test
